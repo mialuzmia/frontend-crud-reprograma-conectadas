5f0bbfbe665109d079a66d548fb68fffca5f9b2c
TainaRaio <126360488+TainaRaio@users.noreply.github.com>
2023-11-07 22:05:26 -0300

    sidebar criada

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,30 @@
 // styles
 import styles from "../styles/components/Sidebar.module.css";
+import { House, Plus, UserCircle, ArrowLeft } from "@phosphor-icons/react";
+import { Link } from 'react-router-dom';
+
 
 const Sidebar = () => {
   return (
-    <div className={styles.sidebar__container}>oi</div>
+    <nav className={styles.sidebar__container}>
+      <Link to="/" className={styles.sidebar__backButton}>
+        <ArrowLeft size={30} color="#fff" weight="bold"/>
+      </Link>
+      <div className={styles.sidebar__pageIcons}>
+        <Link to="/">
+          <House size={30} color="#fff" weight="bold"/>
+        </Link>
+        <Link to="/cadastrar">
+          <Plus size={30} color="#fff" weight="bold"/>
+        </Link>
+        <Link to="/">
+          <UserCircle size={30} color="#fff" weight="bold"/>
+        </Link>
+      </div>
+
+    </nav>
   )
 }
 
 export default Sidebar
+
