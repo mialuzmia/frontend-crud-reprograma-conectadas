89319a94e8eeb8f74d08aab180fb39711c4fe686
TainaRaio <126360488+TainaRaio@users.noreply.github.com>
2023-11-10 17:47:38 -0300

    pagina devs pronta

--- src/pages/sobre/index.jsx
+++ src/pages/sobre/index.jsx
@@ -1,7 +1,65 @@
 import styles from "../../styles/pages/Sobre.module.css";
+import imgtai from "../../assets/imagens/tai.jpg";
+import imgsarah from "../../assets/imagens/sarah.jpeg";
+import imgjamile from "../../assets/imagens/jamile.jpeg"
+import imgmia from "../../assets/imagens/mia.jpeg"
+import CardDev from "../../components/CardDev";
+
+// import dev from "../../dev.json"
+
+
 
 const Sobre = () => {
-  return <div className={`page ${styles.sobre__container}`}>Sobre</div>;
-};
 
-export default Sobre;
+  const devs = [
+      {key: "1", 
+      image: imgtai,
+      name: "Tainã Raio",
+      subtitlle: "Front-end",
+      description: "Gosto de acender uma vela aromatizada no final do dia e relaxar. Amo cantar, dançar e assistir série junto com uma comidinha gostosa",
+      github: "https://github.com/TainaRaio"},
+      {key: "2", 
+      image: imgmia,
+      name: "Mia Azevedo",
+      subtitlle: "Front-end",
+      description: " Gosto de de livros de fantasia, animações (desenhos e animes), jogos antigos e da minha gata",
+      github: "https://www.linkedin.com/in/mialuzmia/"},
+      {key: "3", 
+      image: imgsarah,
+      name: "Sarah",
+      subtitlle: "Back-end",
+      description: "Gosto de ",
+      github: "https://www.linkedin.com/in/sarah-bernardino/"},
+      {key: "4", 
+      image: imgjamile,
+      name: "Jamile",
+      subtitlle: "Back-end",
+      description: " Gosto de Rock e filmes de terror, nos finais de semanas gostei de tomar um vinho vegano em casa, assistir animes e comer acarajé. Meus hobbies são basicamente esses: ficar em casa, com minhas filhas, ouvir Rock, ver filmes e séries de terror e muito anime...  ",
+      github: "https://www.linkedin.com/in/jamile-santana-da-silva/"}
+  ];
+
+  return (
+    
+    <div className={styles.containerDevs}>
+      {devs.map((dev) => (
+        <CardDev
+          key={dev.key} 
+          image={dev.image}
+          name={dev.name}
+          subtitlle={dev.subtitlle}
+          description={dev.description}
+          github={dev.github}
+
+        />
+       
+
+      )
+      
+      )}
+
+    </div>
+    
+  )
+}
+
+export default Sobre
