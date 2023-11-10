50ba1580c9d443bb57dd7ecae057e332c204c3e5
TainaRaio <126360488+TainaRaio@users.noreply.github.com>
2023-11-10 18:16:02 -0300

    pagina devs pronta e modificada

--- src/components/CardDev.jsx
+++ src/components/CardDev.jsx
@@ -3,14 +3,14 @@ import { GithubLogo } from '@phosphor-icons/react';
 
 const CardDev = ({ id, image, name, subtitlle, description, github }) => {
   return (
-    <div className={styles.cardDevs} key={id}>
+    <div className={styles.card__devs} key={id}>
       <div>
         <img className={styles.card__image} src={image}/>
       </div>
-        <h2 className={styles.cardTitle}>{name}</h2>
+        <h2 className={styles.card__title}>{name}</h2>
         <h3 className={styles.card__subtitle}>{subtitlle}</h3>
         <h4 className={styles.card__description}>{description}</h4>
-        <a className={styles.cardLink} href={github}>
+        <a className={styles.card__link} href={github}>
         <GithubLogo size={20} />
         </a>
     </div>
